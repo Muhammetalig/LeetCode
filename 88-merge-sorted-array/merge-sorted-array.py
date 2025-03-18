3445e66f5226036b6e92d2da913d30dc3b113b60
Muhammet Ali Taşdemir <108293495+Muhammetalig@users.noreply.github.com>
2025-03-18 22:04:14 +0300

    Update merge-sorted-array.py

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,14 +1,14 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        i = m - 1  # nums1'in son geçerli elemanının indexi
-        j = n - 1  # nums2'nin son elemanının indexi
-        k = m + n - 1  # nums1'in toplam uzunluğundaki son index
+        i = m - 1 
+        j = n - 1 
+        k = m + n - 1 
 
-        while j >= 0:  # nums2 tamamen yerleştirilene kadar devam et
-            if i >= 0 and nums1[i] > nums2[j]:  # nums1'in elemanı daha büyükse
-                nums1[k] = nums1[i]  # nums1'in sonundaki boş yere yerleştir
-                i -= 1  # nums1'in indexini azalt
-            else:  # nums2'nin elemanı daha büyükse veya nums1 elemanları tükendiyse
-                nums1[k] = nums2[j]  # nums2'nin elemanını yerleştir
-                j -= 1  # nums2'nin indexini azalt
-            k -= 1  # nums1'in yerleştirme indexini azalt
+        while j >= 0: 
+            if i >= 0 and nums1[i] > nums2[j]: 
+                nums1[k] = nums1[i] 
+                i -= 1  
+            else:
+                nums1[k] = nums2[j]  
+                j -= 1
+            k -= 1  
